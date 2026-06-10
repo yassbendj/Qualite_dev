97c2131906013b524dd3c4a56265cfa141ba3843
yasmine <yasminebendjebbour@gmail.com>
2026-06-10 15:34:28 +0200

    testSetId

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -18,13 +18,9 @@ public class VoitureTest {
     }
 
     @Test
-
     void testSetId()
     {
         car.setId(100);
         assertEquals(100, car.getId(), "Le setter d'ID n'a pas correctement modifié la valeur.");
     }
-
-
-
 }

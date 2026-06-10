a4ef277f20dbb0f38cd2ee47d8898e76f66b8abf
yasmine <yasminebendjebbour@gmail.com>
2026-06-10 15:40:46 +0200

    Service

--- src/main/java/com/example/demo/service/Statistique.java
+++ src/main/java/com/example/demo/service/Statistique.java
@@ -1,11 +1,8 @@
 package com.example.demo.service;
-
 import com.example.demo.data.Voiture;
 
-public interface Statistique {
-
+public interface Statistique
+{
     public void ajouter(Voiture voiture);
-
     public Echantillon prixMoyen() throws ArithmeticException;
-
 }
